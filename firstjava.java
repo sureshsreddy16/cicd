59917c6926c901786ff208374114b72b471ab689
sureshsreddy16 <57609555+sureshsreddy16@users.noreply.github.com>
2019-11-19 09:05:15 +0530

    Create firstjava.java

---
+++ firstjava.java
@@ -0,0 +1,10 @@
+
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class firstjava {
+  public static void main(String[] args) {
+    System.out.println("This is my first batch ");
+
+  }
+}
